d5f930af41fccf1cb80261458ed1a07098abcb3f
Ebi Igweze <ebi.igweze@gmail.com>
2023-01-09 23:49:57 +0100

    sets up showlisting fragment with navigation to shoe-details fragment

---
+++ app/src/main/java/com/udacity/shoestore/viewmodels/ShoeListingViewModel.kt
@@ -0,0 +1,104 @@
+package com.udacity.shoestore.viewmodels
+
+import androidx.lifecycle.ViewModel
+import com.udacity.shoestore.models.Shoe
+
+class ShoeListingViewModel: ViewModel() {
+
+    private val _shoes: MutableList<Shoe> by lazy {
+        defaultShoeList.toMutableList()
+    }
+    val shoes: List<Shoe>
+        get() = _shoes
+
+
+    companion object {
+
+        private val defaultShoeList = listOf(
+            Shoe(
+                name = "Air Zoom Vapor Pro",
+                size = 45.5,
+                company = "Nike",
+                description = "Air Zoom Vapor Pro carpet shoe men - White, Black",
+                images = emptyList()
+            ),
+            Shoe(
+                name = "Air Zoom Vapor",
+                size = 45.0,
+                company = "Nike",
+                description = "Air Zoom Vapor all court tennis shoe men - Black, Dark Grey",
+                images = emptyList()
+            ),
+            Shoe(
+                name = "Court Jam",
+                size = 38.5,
+                company = "Adidas",
+                description = "Court Jam all court tennis shoe women - White, Silver",
+                images = emptyList()
+            ),
+            Shoe(
+                name = "Barricade",
+                size = 42.0,
+                company = "Adidas",
+                description = "Barricade All court shoe kids - Dark Blue, White",
+                images = emptyList()
+            ),
+            Shoe(
+                name = "Adizero Ubersonic",
+                size = 43.0,
+                company = "Adidas",
+                description = "Adizero Ubersonic all court shoe women - White, Violet",
+                images = emptyList()
+            ),
+            Shoe(
+                name = "CourtJam",
+                size = 40.0,
+                company = "Adidas",
+                description = "CourtJam Control carpet shoe women - White, Turquoise",
+                images = emptyList()
+            ),
+            Shoe(
+                name = "Zoom Vapor Cage 4 Pros",
+                size = 42.0,
+                company = "Nike",
+                description = "Zoom Vapor Cage 4 Rafa all court shoe men - Black, Neon Green",
+                images = emptyList()
+            ),
+            Shoe(
+                name = "Zoom NXT",
+                size = 39.5,
+                company = "Nike",
+                description = "Zoom NXT all court shoe women - Black, Ecru",
+                images = emptyList()
+            ),
+            Shoe(
+                name = "Court Vapor Lite",
+                size = 44.5,
+                company = "Nike",
+                description = "Court Vapor Lite all court shoe men - Black, White",
+                images = emptyList()
+            ),
+            Shoe(
+                name = "Game Court 2",
+                size = 44.5,
+                company = "Adidas",
+                description = "Game Court 2 all court shoe women - White, Silver",
+                images = emptyList()
+            ),
+            Shoe(
+                name = "Court Jam Control",
+                size = 44.5,
+                company = "Adidas",
+                description = "Court Jam Control all court shoe women - Green, White",
+                images = emptyList()
+            ),
+            Shoe(
+                name = "Air Zoom Vapor Pro",
+                size = 44.5,
+                company = "Nike",
+                description = "Air Zoom Vapor Pro clay court shoe women - Black, Gold",
+                images = emptyList()
+            ),
+        )
+    }
+}
--- app/src/main/java/com/udacity/shoestore/views/ShoeListingFragment.kt
+++ app/src/main/java/com/udacity/shoestore/views/ShoeListingFragment.kt
@@ -4,18 +4,21 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.LinearLayout
+import androidx.core.widget.NestedScrollView
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.udacity.shoestore.R
 import com.udacity.shoestore.databinding.FragmentShoeListingBinding
+import com.udacity.shoestore.databinding.ListItemShoeBinding
+import com.udacity.shoestore.viewmodels.ShoeListingViewModel
 
-class ShoeListingFragment : Fragment() {
+class ShoeListingFragment : Fragment(), NestedScrollView.OnScrollChangeListener {
 
-    lateinit var binding: FragmentShoeListingBinding
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
+    private lateinit var binding: FragmentShoeListingBinding
+    private val viewModel: ShoeListingViewModel by viewModels()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -25,6 +28,59 @@ class ShoeListingFragment : Fragment() {
         binding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_shoe_listing, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        // listen for scroll events
+        binding.scrollView.setOnScrollChangeListener(this)
+        // navigate to shoe details fragment
+        binding.fab.setOnClickListener {
+            findNavController().navigate(R.id.action_shoeListingFragment_to_shoeDetailFragment)
+        }
+
+        addShoesItemViews()
+    }
+
+    private fun addShoesItemViews() {
+        val inflater = LayoutInflater.from(requireContext())
+        viewModel.shoes.forEach { shoe ->
+            // inflate shoe list item view
+            val shoeListItem: ListItemShoeBinding = DataBindingUtil.inflate(
+                inflater, R.layout.list_item_shoe, null, false)
+            // set the shoe variable binding
+            shoeListItem.shoe = shoe
+            // add the list item to layout
+            binding.shoeListingContainer.addView(shoeListItem.root)
+        }
+    }
+
+    override fun onScrollChange(
+        v: NestedScrollView,
+        scrollX: Int,
+        scrollY: Int,
+        oldScrollX: Int,
+        oldScrollY: Int
+    ) {
+
+        with(binding) {
+            // the delay of the extension of the FAB is set for 12 items
+            if (scrollY > oldScrollY + 12 && fab.isExtended) {
+                fab.shrink()
+            }
+
+            // the delay of the extension of the FAB is set for 12 items
+            if (scrollY < oldScrollY - 12 && !fab.isExtended) {
+                fab.extend()
+            }
+
+            // if the nestedScrollView is at the first item of the list then the
+            // extended floating action should be in extended state
+            if (scrollY == 0) {
+                fab.extend()
+            }
+
+        }
     }
 
 }
